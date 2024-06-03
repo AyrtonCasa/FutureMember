ee815f97442947492aaf109cd03c10f231d520e2
AyrtonCasa <ayrton.casa@sptech.school>
2024-06-03 14:51:11 -0300

    Pequenos Ajustes

--- Site Institucional/src/routes/publi.js
+++ Site Institucional/src/routes/publi.js
@@ -20,27 +20,13 @@ router.post("/publicar/:idUsuario", function (req, res) {
 });
 
 router.post("/curtir/:idPublicacao/:idUsuario", function (req, res) {
-    // var idPublicacao = req.params.idPublicacao;
-    // var idUsuario = req.params.idUsuario;
-    
     publiController.curtir(req, res);
 });
 
 router.post("/VerCurtida/", function (req, res) {
-    // const { idPublicacao, idUsuario } = req.params;
-
     console.log("TO NA ROTA")
     publiController.VerCurtida(req, res);
 
-    // const curtidaExiste = curtidas.some(curtida => 
-    //     curtida.idPublicacao === parseInt(idPublicacao) && curtida.idUsuario === parseInt(idUsuario)
-    // );
-
-    // if (curtidaExiste) {
-    //     res.status(200).send({ curtido: true });
-    // } else {
-    //     res.status(200).send({ curtido: false });
-    // }
 });
 
 router.delete("/deletarCurtida/:idPublicacao/:idUsuario", function (req, res) {
